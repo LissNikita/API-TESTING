35a50fcd239637086ed7f629b88f5963520a151d
Nik Lis <dragon.lisov@mail.ru>
2023-05-01 15:11:32 +0300

    changes

--- src/test/java/models/Geo.java
+++ src/test/java/models/Geo.java
@@ -5,46 +5,4 @@ import lombok.Data;
 public class Geo {
     private String lat;
     private String lng;
-
-//    public Geo(String lat, String lng) {
-//        this.lat = lat;
-//        this.lng = lng;
-//    }
-//
-//    public Geo() {
-//    }
-//
-//    public String getLat() {
-//        return lat;
-//    }
-//
-//    public String getLng() {
-//        return lng;
-//    }
-//
-//    @Override
-//    public String toString() {
-//        return "Geo{" +
-//                "lat='" + lat + '\'' +
-//                ", lng='" + lng + '\'' +
-//                '}';
-//    }
-//
-//    @Override
-//    public boolean equals(Object o) {
-//        if (this == o) return true;
-//        if (o == null || getClass() != o.getClass()) return false;
-//
-//        Geo geo = (Geo) o;
-//
-//        if (!Objects.equals(lat, geo.lat)) return false;
-//        return Objects.equals(lng, geo.lng);
-//    }
-//
-//    @Override
-//    public int hashCode() {
-//        int result = lat != null ? lat.hashCode() : 0;
-//        result = 31 * result + (lng != null ? lng.hashCode() : 0);
-//        return result;
-//    }
 }
